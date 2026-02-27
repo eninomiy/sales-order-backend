c9b3da8ea239a09543f6200f7112795b2024b5ca
eninomiy <eduninomiya@yahoo.com.br>
2026-02-27 17:47:29 -0300

    feat: add path to cds-models

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,14 @@
+import { Customer, Customers } from '@models/sales';
+
+const customer: Customer = {
+    email: 'john.doe@example.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    phoneNumber: '+1234567890',
+};
+
+const cutomers: Customers = [customer]
+
 const funcao = (variavel: string) => console.log(variavel);
 
 funcao('Hello World');
